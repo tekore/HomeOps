50625edb402eb26eb031db765d02b02a97afbca5
tekore <tekore@tuta.com>
2025-05-03 17:22:33 +0200

    overhaul so this wont rely on community frameworks

--- Auto-Install/README.md
+++ Auto-Install/README.md
@@ -1,12 +1,12 @@
-## Proxmox Auto Install
+## Proxmox Auto Install (with a GitHub Actions runner install)
 
 ## Contents
-- [Prerequisites](#Prerequisits)
-- [How-To](#How-To)
+- [Prerequisites](##Prerequisits)
+- [How-To](##How-To)
 
 ## Prerequisites
-- You have a USB of atleast 8GB in size
-- A Linux host you can use to create the USB (Commands may vary slightly if not Ubuntu 24.04)
+- You have two USB drives (one of atleast 8GB in capacity)
+- A Linux host you can use to create the bootable USB (Commands may vary slightly if you're not using Ubuntu 24.04)
 
 ## How-To
 1. Add the repo for the 'proxmox-auto-install-assistant' tool (if applicable)
@@ -19,23 +19,31 @@ echo "deb [arch=amd64] http://download.proxmox.com/debian/pve bookworm pve-no-su
 sudo apt install proxmox-auto-install-assistant -y 
 ```
 
-3. Customise the answer.toml file
-[answer.toml](https://github.com/tekore/HomeOps/blob/main/auto-install/answer.toml)
+3. Customise the answer.toml file values (Change the url unless you want to use my script)
+[answer.toml](https://github.com/tekore/HomeOps/blob/main/Auto-Install/answer.toml)
 
-4. Customise the first-boot-script.sh file
-[first-boot-script.sh](https://github.com/tekore/HomeOps/blob/main/auto-install/first-boot-script.sh)
+4. Customise the variables in the first-boot-script.sh file (Change ANSIBLE_REPO_URL and ANSIBLE_REPO_PLAYBOOK unless you want to use my playbook)
+[first-boot-script.sh](https://github.com/tekore/HomeOps/blob/main/Auto-Install/first-boot-script.sh)
 
 5. Validate your answer.toml file
 ```sh
 proxmox-auto-install-assistant validate-answer answer.toml
 ```
 
-6. Download the Proxmox-VE ISO
-[Proxmox_Download](https://www.proxmox.com/en/downloads)
+6. Download the Proxmox-VE_8 ISO
+[Proxmox_Download](https://enterprise.proxmox.com/iso/proxmox-ve_8.4-1.iso)
 
 7. Build your custom ISO
 ```sh
-proxmox-auto-install-assistant prepare-iso ./proxmox-ve*.iso --fetch-from iso --answer-file ./answer.toml --on-first-boot ./first-boot-script.sh
+proxmox-auto-install-assistant prepare-iso ./proxmox-ve*.iso --fetch-from iso --answer-file ./answer.toml
 ```
 
-8. Now burn the newly created ISO image to a USB (using a tool such as 'balenaEtcher') and boot from it.
+8. Now burn the newly created ISO image to a USB (using a tool such as 'balenaEtcher')
+[balenaEtcher_Download](https://etcher.balena.io/#download-etcher)
+
+## (If you don't want to GitHub runner installed you can stop at this stage and just boot from the aforementioned USB)
+9. Customise the runner_secrets.yml and copy it onto the second USB
+[runner_secrets_example.yml](https://github.com/tekore/HomeOps/blob/main/Auto-Install/runner_secrets_example.yml)
+
+10. Plug both USB drives into the server and boot from the one used in step 8.\
+Once the install is complete, remove the boot USB (as you will be prompted to do so) but leave the second USB plugged in.
--- Auto-Install/first-boot-script.sh
+++ Auto-Install/first-boot-script.sh
@@ -1,16 +1,86 @@
 #!/bin/bash
 
-# Exit on error
 set -e
 
-# Remove the enterprise repositories
-rm -f /etc/apt/sources.list.d/*list
+# === Variables ===
+VM_ID=$((100 + RANDOM % 899))
+VM_NAME="GitHub_Runner"
+BRIDGE="vmbr0"
+STORAGE="local-zfs"
+CLOUD_IMAGE_URL="https://cloud-images.ubuntu.com/releases/22.04/release/ubuntu-22.04-server-cloudimg-amd64.img"
+USER="runner"
+PASSWORD="Changeme123"
+SSH_KEY_PATH="$HOME/.ssh/id_rsa.pub"
+ANSIBLE_REPO_URL="https://github.com/tekore/HomeOps.git"
+ANSIBLE_REPO_PLAYBOOK="Ansible/configure-runner.yml"
+CLOUDINIT_DIR="/var/lib/vz/template"
+CLOUDINIT_FILE="${CLOUDINIT_DIR}/${VM_NAME}-${VM_ID}-cloudinit.yaml"
 
-# Install required packages
-apt-get update
-apt-get install -y ansible git
+# === Search for the required USB drive/file===
+echo "🔎 Searching for USB drive.."
+found_usb=0
 
-# Create ansible-pull directory if it doesn't exist
-mkdir -p /opt/ansible-pull
+for DEV in /dev/sd?; do
+  if [[ ! -b $DEV ]]; then
+    continue
+  fi
+  if udevadm info --query=property --name="$DEV" | grep -q "ID_BUS=usb"; then
+    found_usb=1
+    echo "✅ Found USB drive $DEV"
+    mount $DEV /mnt
+    for yaml_file in /mnt/*.yml; do
+          if [[ -f "$yaml_file" ]]; then
+            echo "✅ Found YAML file: $yaml_file"
+            yaml_data+=$'\n'
+            yaml_data+="$(cat "$yaml_file")"
+          fi
+        done
+  fi
+done
 
-ansible-pull -U https://github.com/tekore/HomeOps.git -d /opt/ansible-pull --purge Ansible/configure-runner.yml
+if [[ $found_usb -eq 0 ]]; then
+  echo "❌ No USB drives detected!"
+  exit 1
+fi
+
+# === Download the Ubuntu cloud image ===
+wget -O ubuntu_cloud.img "$CLOUD_IMAGE_URL"
+echo "✅ Downloaded ubuntu Cloud Image"
+# === CREATE VM ===
+qm create $VM_ID --name $VM_NAME --memory 2048 --net0 virtio,bridge=$BRIDGE
+
+# === Import the disk ===
+qm importdisk $VM_ID ubuntu_cloud.img $STORAGE
+qm set $VM_ID --scsihw virtio-scsi-pci --scsi0 $STORAGE:vm-${VM_ID}-disk-0
+
+# === Configure the cloud-init drive ===
+qm set $VM_ID --ide2 $STORAGE:cloudinit
+qm set $VM_ID --boot order=scsi0
+qm set $VM_ID --serial0 socket --vga serial0
+
+# TODO: After testing, remove the runner_secrets.yml out of /tmp
+# === Create the cloud-init file ===
+mkdir -p "$CLOUDINIT_DIR"
+cat > "$CLOUDINIT_FILE" <<EOF
+#cloud-config
+package_update: true
+write_files:
+  - path: /tmp/runner_secrets.yml
+    content: |
+      $yaml_data
+    owner: 'root:root'
+    permissions: '0644'
+packages:
+  - ansible
+runcmd:
+  - ansible-pull -U $ANSIBLE_REPO_URL -i localhost --purge $ANSIBLE_REPO_PLAYBOOK --extra-vars "@runner_secrets.yml"
+EOF
+
+# === Apple cloud-init settings ===
+qm set $VM_ID --ciuser $USER --cipassword "$PASSWORD" --ipconfig0 ip=dhcp
+qm set $VM_ID --sshkey "$SSH_KEY_PATH"
+qm set $VM_ID --cicustom "user=local:template/$(basename $CLOUDINIT_FILE)"
+
+# === START VM ===
+qm start $VM_ID
+echo "✅ VM $VM_ID ($VM_NAME) created and started with Ansible pull."
