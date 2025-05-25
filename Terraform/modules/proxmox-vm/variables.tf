91f91d9206460ceb548628c08275bd6fdb1e1e75
tekore <tekore@tuta.com>
2025-05-25 16:25:34 +0200

    Converted to a terraform module, added playbook to create a router, pushing as to backup, not finished yet

--- Terraform/modules/proxmox-vm/variables.tf
+++ Terraform/modules/proxmox-vm/variables.tf
@@ -10,23 +10,23 @@ variable "vm_description" {
 }
 
 variable "vm_tags" {
-  description = "Tags to apply to the virtual machine"
+  description = "Virtual machine tags"
   type        = list(string)
   default     = ["Terraform"]
 }
 
 variable "node_name" {
-  description = "Proxmox node name where the VM will be created"
+  description = "Proxmox node"
   type        = string
 }
 
 variable "vm_id" {
-  description = "VM ID number"
+  description = "VM ID"
   type        = number
 }
 
 variable "host_pci_device" {
-  description = "PCI device to pass through"
+  description = "PCI device"
   type = object({
     device  = string
     id = string
@@ -47,13 +47,13 @@ variable "agent_enabled" {
 }
 
 variable "stop_on_destroy" {
-  description = "Stop VM on destroy"
+  description = "Stop on destroy"
   type        = bool
   default     = true
 }
 
 variable "startup_order" {
-  description = "VM startup order"
+  description = "Startup order"
   type        = string
   default     = "3"
 }
@@ -83,25 +83,25 @@ variable "cpu_type" {
 }
 
 variable "memory_dedicated" {
-  description = "Dedicated memory in MB"
+  description = "Dedicated RAM in MB"
   type        = number
   default     = 2048
 }
 
 variable "memory_floating" {
-  description = "Floating memory in MB"
+  description = "Floating RAM in MB (for balooning)"
   type        = number
   default     = 2048
 }
 
 variable "datastore_id" {
-  description = "Datastore ID for VM disk and initialization"
+  description = "Datastore ID"
   type        = string
   default     = "local-zfs"
 }
 
 variable "disk_file_id" {
-  description = "File ID for the VM disk image"
+  description = "File ID for disk"
   type        = string
 }
 
@@ -112,31 +112,31 @@ variable "disk_interface" {
 }
 
 variable "ip_address" {
-  description = "IP address configuration (use 'dhcp' for DHCP)"
+  description = "IP address (can use DHCP)"
   type        = string
   default     = "dhcp"
 }
 
 variable "username" {
-  description = "Username for the VM user account"
+  description = "User account"
   type        = string
   default     = "ubuntu"
 }
 
 variable "password" {
-  description = "Password for the VM user account"
+  description = "Password"
   type        = string
   sensitive   = true
 }
 
 variable "network_bridge" {
-  description = "Network bridge for VM"
+  description = "Virtual machine network"
   type        = string
   default     = "vmbr0"
 }
 
 variable "os_type" {
-  description = "Operating system type"
+  description = "OS type"
   type        = string
   default     = "l26"
 }
