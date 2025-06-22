73f33d57bd58b0851ad47fac89731d49c97a8323
tekore <tekore@tuta.com>
2025-06-22 13:16:00 +0200

    added lifecycle to ignore manual user changes to acl

--- Terraform/users.tf
+++ Terraform/users.tf
@@ -3,6 +3,9 @@ resource "proxmox_virtual_environment_user" "tekore" {
   comment  = "Managed by Terraform"
   password = var.passwords.tekore
   user_id  = "tekore@pve"
+  lifecycle {
+    ignore_changes = [acl]
+  }
 }
 
 resource "proxmox_virtual_environment_acl" "tekore_acl" {
@@ -10,6 +13,7 @@ resource "proxmox_virtual_environment_acl" "tekore_acl" {
   role_id = "PVEAdmin"
   path      = "/"
   propagate = true
+  depends_on = [ proxmox_virtual_environment_user.tekore ]
 }
 
 // valraevn
@@ -17,6 +21,9 @@ resource "proxmox_virtual_environment_user" "valraevn" {
   comment  = "Managed by Terraform"
   password = var.passwords.valraevn
   user_id  = "valraevn@pve"
+  lifecycle {
+    ignore_changes = [acl]
+  }
 }
 
 resource "proxmox_virtual_environment_acl" "valraevn_acl" {
@@ -24,4 +31,5 @@ resource "proxmox_virtual_environment_acl" "valraevn_acl" {
   role_id = "PVEAdmin"
   path      = "/"
   propagate = true
+  depends_on = [ proxmox_virtual_environment_user.valraevn ]
 }
