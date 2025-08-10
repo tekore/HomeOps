ee4c6be30977cc213988681bcd0458154135643b
tekore <tekore@tutamail.com>
2025-08-10 13:02:05 +0200

    new interface

--- Terraform/network.tf
+++ Terraform/network.tf
@@ -9,11 +9,11 @@ resource "proxmox_virtual_environment_network_linux_bridge" "vmbr99" {
 resource "proxmox_virtual_environment_network_linux_bridge" "vmbr101" {
   node_name = data.proxmox_virtual_environment_node.node.node_name
   name      = "vmbr101"
-  address = "192.168.101.100"
+  address = "192.168.101.100/24"
   gateway = "192.168.101.1"
   vlan_aware = true
   ports = [ proxmox_virtual_environment_network_linux_vlan.vlan101.name ]
-  depends_on = [ proxmox_virtual_environment_network_linux_vlan.vlan99 ]
+  depends_on = [ proxmox_virtual_environment_network_linux_vlan.vlan101 ]
 }
 
 resource "proxmox_virtual_environment_network_linux_vlan" "vlan101" {
